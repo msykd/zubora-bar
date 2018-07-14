80a214941636e225889ff5052f5e9e176039a77d
Masaya Kudo <msykd16@gmail.com>
2018-07-14 14:16:49 +0900

    indexページ修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'ズボラバー',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>ズボラバー</h1>
+    <p>まだなにもありません。ズボラなので。</p>
   </div>
-)
+);
 
 export default IndexPage
